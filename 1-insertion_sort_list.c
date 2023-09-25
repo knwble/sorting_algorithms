14d2e35fbcfa8658c41ccf1bf2aa8c8f102422c5
knwble <noble4peter1@gmail.com>
2023-09-25 20:29:51 +0100

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,57 +1,45 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swaps two nodes in a listint_t doubly-linked list.
- * @h: A pointer to the head of the doubly-linked list.
- * @n1: A pointer to the first node to swap.
- * @n2: The second node to swap.
- */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
-{
-	/* Adjust next pointers */
-	(*n1)->next = n2->next;
-	if (n2->next != NULL)
-		n2->next->prev = *n1;
-
-	/* Adjust prev pointers */
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-
-	/* Update head pointer if necessary */
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
-	else
-		*h = n2;
-
-	/* Update prev pointer of n1 */
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
-}
-
-/**
- * insertion_sort_list - Sorts a doubly linked list of integers
- *                       using the insertion sort algorithm.
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion Sort algorithm.
  * @list: A pointer to the head of a doubly-linked list of integers.
  *
- * Description: Prints the list after each swap operation.
+ * Description: Prints the list after each time you swap two elements.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+    listint_t *current, *prev, *temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
-	{
-		tmp = iter->next;
-		insert = iter->prev;
+    current = (*list)->next;
 
-		/* Perform swaps while checking values */
-		while (insert != NULL && iter->n < insert->n)
-		{
-			swap_nodes(list, &insert, iter);
-			print_list((const listint_t *)*list);
-		}
-	}
+    while (current != NULL)
+    {
+        prev = current->prev;
+        while (prev != NULL && prev->n > current->n)
+        {
+            temp = prev->prev;
+            if (temp != NULL)
+                temp->next = current;
+            else
+                *list = current;
+
+            prev->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = prev;
+
+            current->prev = temp;
+            current->next = prev;
+            prev->prev = current;
+
+            print_list((const listint_t *)*list);
+
+            prev = temp;
+        }
+
+        current = current->next;
+    }
 }
