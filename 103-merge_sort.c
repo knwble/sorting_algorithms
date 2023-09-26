439a10034d0945104473ad5f943cb055255cbb25
knwble <noble4peter1@gmail.com>
2023-09-26 15:55:06 +0100

    merge sort update

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,5 @@
 #include "sort.h"
 
-
 /**
  * merge - Merge two subarrays into one sorted array
  * @array: The original array to merge into
@@ -8,46 +7,35 @@
  * @left: Pointer to the left subarray
  * @right: Pointer to the right subarray
  */
-void merge(int *array, size_t size, int *left, int *right)
+static void merge(int *array, size_t size, int *left, int *right)
 {
+	size_t i = 0, j = 0, k = 0;
+	size_t left_size = size / 2;
+	size_t right_size = size - left_size;
+	int *temp = malloc(sizeof(int) * size);
 
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(left, left_size);
-	printf("[right]: ");
-	print_array(right, right_size);
+	if (!temp)
+		exit(EXIT_FAILURE);
 
 	while (i < left_size && j < right_size)
 	{
 		if (left[i] <= right[j])
-		{
-			array[k] = left[i];
-			i++;
-		}
+			temp[k] = left[i++];
 		else
-		{
-			array[k] = right[j];
-			j++;
-		}
+			temp[k] = right[j++];
 		k++;
 	}
 
 	while (i < left_size)
-	{
-		array[k] = left[i];
-		i++;
-		k++;
-	}
+		temp[k++] = left[i++];
 
 	while (j < right_size)
-	{
-		array[k] = right[j];
-		j++;
-		k++;
-	}
+		temp[k++] = right[j++];
 
-	printf("[Done]: ");
-	print_array(array, size);
+	for (i = 0; i < size; i++)
+		array[i] = temp[i];
+
+	free(temp);
 }
 
 /**
@@ -58,14 +46,39 @@ void merge(int *array, size_t size, int *left, int *right)
  */
 void merge_sort(int *array, size_t size)
 {
-	if (size < 2 || array == NULL)
+	if (!array || size < 2)
 		return;
 
 	size_t middle = size / 2;
 	int *left = array;
 	int *right = array + middle;
 
+	printf("Merging...\n[left]: ");
+	for (size_t i = 0; i < middle; i++)
+	{
+		printf("%d", left[i]);
+		if (i < middle - 1)
+			printf(", ");
+	}
+	printf("\n[right]: ");
+	for (size_t i = 0; i < size - middle; i++)
+	{
+		printf("%d", right[i]);
+		if (i < size - middle - 1)
+			printf(", ");
+	}
+	printf("\n");
+
 	merge_sort(left, middle);
 	merge_sort(right, size - middle);
 	merge(array, size, left, right);
+
+	printf("[Done]: ");
+	for (size_t i = 0; i < size; i++)
+	{
+		printf("%d", array[i]);
+		if (i < size - 1)
+			printf(", ");
+	}
+	printf("\n");
 }
