e27e5001a6e1be8fa22d09ad2b5887a345f05a7d
knwble <noble4peter1@gmail.com>
2023-09-27 11:51:37 +0100

    header files updated

--- sort.h
+++ sort.h
@@ -47,7 +47,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size, int exp);
 void merge_sort(int *array, size_t size);
 void merge_subarr(int *subarr, int *buff, size_t front, size_t mid,
 		size_t back);
